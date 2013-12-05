402b44dadc2b23414667490f51a3c0ff468f7169
frankirox <fonseca.franchesco@gmail.com>
2013-12-04 22:03:35 -0430

    Basic Status Action in acording with the documentation

--- Action/StatusAction.php
+++ Action/StatusAction.php
@@ -1,55 +1,41 @@
 <?php
-namespace Payum\Be2Bill\Action;
+namespace App\Payum\Action;
 
-use Payum\Core\Action\ActionInterface;
-use Payum\Core\Bridge\Spl\ArrayObject;
-use Payum\Core\Request\StatusRequestInterface;
-use Payum\Core\Exception\RequestNotSupportedException;
-use Payum\Be2Bill\Api;
+use Payum\Action\ActionInterface;
+use Payum\Request\StatusRequestInterface;
 
 class StatusAction implements ActionInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function execute($request)
     {
-        /** @var $request StatusRequestInterface */
-        if (false == $this->supports($request)) {
-            throw RequestNotSupportedException::createActionNotSupported($this, $request);
-        }
+        $model = $request->getModel();
 
-        $model = new ArrayObject($request->getModel());
-        
-        if (null === $model['EXECCODE']) {
+        if (false == isset($model['status'])) {
             $request->markNew();
-            
+
             return;
         }
-        
-        if (Api::EXECCODE_SUCCESSFUL === $model['EXECCODE']) {
+
+        if ('success' == $model['status']) {
             $request->markSuccess();
 
             return;
         }
-        
-        if (Api::EXECCODE_TIME_OUT  === $model['EXECCODE']) {
-            $request->markUnknown();
-            
+
+        if ('error' == $model['status']) {
+            $request->markFailed();
+
             return;
         }
 
-        $request->markFailed();
+        $request->markUnknown();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function supports($request)
     {
         return
             $request instanceof StatusRequestInterface &&
             $request->getModel() instanceof \ArrayAccess
-        ;
+            ;
     }
 }
