17a0fa4653c256d3ff3189cb3cc7f7f929396a0d
David Baker <dbaker@acorncomputersolutions.com>
2013-12-19 16:02:16 -0700

    Added a rough status action - no real work has been done here

---
+++ src/Action/PaymentWithCreditCardStatusAction.php
@@ -0,0 +1,51 @@
+<?php
+namespace Payum\DineroMail\Action;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Request\StatusRequestInterface;
+
+class PaymentWithCreditCardStatusAction implements ActionInterface
+{
+    public function execute($request)
+    {
+        $model = $request->getModel();
+
+        // used by the CC system
+        if ('PENDING' == isset($model['status'])) {
+            $request->markPending();
+
+            return;
+        }
+
+        /* I have doubts here, I think this payment method never gets the COMPLETED status immediately
+        /* (I think this thing applies only for IPN)
+         * */
+        if ('COMPLETED' == $model['status']) {
+            $request->markSuccess();
+
+            return;
+        }
+
+        if ('DENIED' == $model['status']) {
+            $request->markFailed();
+
+            return;
+        }
+
+        if ('ERROR' == $model['status']) {
+            $request->markFailed();
+
+            return;
+        }
+
+        $request->markUnknown();
+    }
+
+    public function supports($request)
+    {
+        return
+            $request instanceof StatusRequestInterface &&
+            $request->getModel() instanceof \ArrayAccess
+            ;
+    }
+}
